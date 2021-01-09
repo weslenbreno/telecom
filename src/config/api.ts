d3d7cd648d9b68564a63899fdecddd0c30574280
Weslen Breno <weslenbreno@gmail.com>
2021-01-09 02:06:26 -0300

    Setup Api Client

---
+++ src/config/api.ts
@@ -0,0 +1,25 @@
+export const BASE_URL = '';
+
+const Client = async (path: string, {body, ...customConfig}: any) => {
+    const config = {
+        ...customConfig,
+        headers: {
+          ...customConfig.headers,
+        },
+    }
+
+    if (body) {
+        config.body = JSON.stringify(body)
+    }
+
+    return fetch(`${BASE_URL}/${path}`, config).then(async response => {
+        const data = await response.json()
+        if (response.ok) {
+          return data
+        } else {
+          return Promise.reject(data)
+        }
+      })
+}
+
+export default Client;
