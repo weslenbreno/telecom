1878bbfb993da91e12e4c778740ff6d68d60e557
Weslen Breno <weslenbreno@gmail.com>
2021-01-09 02:07:00 -0300

    Add Colors constant

---
+++ src/utils/constants/Colors.ts
@@ -0,0 +1,12 @@
+enum Colors {
+    primary = '#0093E9',
+    secondary = "#36C2CF",
+    green = '#43FEAF',
+    black = '#263843',
+    white = '#ffffff',
+    bg = '#F5F5F5',
+    softBlue = '#8EA6C1',
+    red = '#FE4365'
+}
+
+export default Colors;
