bf27e5e7535710eed37657a4e73b39953691472e
Weslen Breno <weslenbreno@gmail.com>
2021-01-10 13:10:52 -0300

    Add pagination and better number format

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import { createServer, Model, Factory } from 'miragejs';
 import generateNumber  from 'utils/scripts/generateDIDNumbers';
 
-
 export function makeServer({ environment = 'test' }) {
   return createServer({
     environment,
@@ -20,6 +19,17 @@ export function makeServer({ environment = 'test' }) {
         value() {
           return generateNumber()
         },
+        monthyPrice () {
+          return (Math.random() * 10).toFixed(2);
+        },
+        setupPrice () {
+          return (Math.random() * 10).toFixed(2);
+        },
+        currency () {
+          const currencies = ['U$', 'R$', '€'];
+          const index = Math.floor(Math.random() * currencies.length)
+          return currencies[index];
+        }
       }),
     },
 
@@ -30,20 +40,27 @@ export function makeServer({ environment = 'test' }) {
     routes() {
       this.namespace = 'api';
       this.get('numbers', (schema, request) => {
-        const {queryParams: { pageOffset = 0, pageSize }} = request
+        let {queryParams: { pageOffset = 1, pageSize }} = request
+        
         const numbers = schema.db.numbers;
+
         if (parseInt(pageSize)) {
-            const start = parseInt(pageSize) * parseInt(pageOffset)
+            
+            const start = parseInt(pageSize) * (parseInt(pageOffset) <= 1 ? 0: pageOffset -1)
             const end = start + parseInt(pageSize)
             const page = numbers.slice(start, end)
+
             return {
                 data: page,
+                total: numbers.length,
                 next: numbers.length > end ? parseInt(pageOffset) + 1 : null,
-                prev: parseInt(pageOffset) - 1 === 0 ? parseInt(pageOffset) - 1 : null
+                prev: parseInt(pageOffset) - 1 === 0 ? null : parseInt(pageOffset) - 1
             }
         }
         return numbers
       });
+      
+      this.resource("numbers", { except: ["index"] })
     },
   });
 }
--- src/utils/scripts/generateDIDNumbers.ts
+++ src/utils/scripts/generateDIDNumbers.ts
@@ -1,7 +1,7 @@
 export const generateNumbers= () => {
-  const randomNumber = String(Math.floor(Math.random() * 110000000000)).padEnd(11, '0');
-  let match = randomNumber.match(/^(\d{2})(\d{5})(\d{4})$/);
-  return match ? `+(${match[1]}) ${match[2]}-${match[3]}`: randomNumber;
+  const randomNumber = String(Math.floor(Math.random() * 130000000000)).padEnd(13, '0');
+  let match = randomNumber.match(/^(\d{2})(\d{2})(\d{5})(\d{4})$/);
+  return match ? `+${match[1]} (${match[2]}) ${match[3]}-${match[4]}`: randomNumber;
 };
 
 export default generateNumbers;
