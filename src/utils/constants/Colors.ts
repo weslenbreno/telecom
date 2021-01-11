9cac64f1401dd2734ed70974c8f1f62696cbf050
Weslen Breno <weslenbreno@gmail.com>
2021-01-11 19:38:03 -0300

    Add new colors

--- src/utils/constants/Colors.ts
+++ src/utils/constants/Colors.ts
@@ -1,12 +1,14 @@
 enum Colors {
     primary = '#0093E9',
+    blue = '#263BA4',
     secondary = "#36C2CF",
     green = '#43FEAF',
     black = '#263843',
     white = '#ffffff',
     bg = '#F5F5F5',
     softBlue = '#8EA6C1',
-    red = '#FE4365'
+    red = '#FE4365',
+    yellow = '#ffeb00'
 }
 
 export default Colors;
