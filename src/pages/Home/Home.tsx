76af563d03fa83ca074446e46d03618ebef6c568
Weslen Breno <weslenbreno@gmail.com>
2021-01-11 19:35:00 -0300

    Change logic to pooling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,60 @@
-import { Header, TelecomCard } from 'components';
-import React from 'react';
+import React, { useState } from 'react';
+import { Header, NumbersTable, SearchNumbers, TelecomCard } from 'components';
 import { Col, Row, Container } from 'react-bootstrap';
 import { Colors } from 'utils';
 import { CardGrid } from './styles';
+import { debounce } from 'lodash';
+import useTotalSetupPrice from 'hooks/useTotalCount';
+import useListNumbersWithPooling from 'hooks/useListNumbersWithPooling';
 
 const Home = () => {
+  const totalCount = useTotalSetupPrice();
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const { numbers } = useListNumbersWithPooling(page, query, 10000);
+
+  const onFiltering = debounce((event: any) => {
+    const query = event.target.value || '';
+    setQuery(query);
+  }, 500);
+
   return (
     <>
       <Header />
-      <Container>
+      <Container className="pb-4">
         <Row>
           <Col>
             <CardGrid>
-              <TelecomCard title="Numbers Available" value={800} />
               <TelecomCard
-                title="Total Budget"
-                value={15.6}
+                title="Numbers Available"
+                value={totalCount.numbers}
+                colors={[Colors.blue, Colors.primary]}
+              />
+              <TelecomCard
+                title="Total Setup Price"
+                value={totalCount.setupPrice}
                 colors={[Colors.green, Colors.primary]}
               />
+              <TelecomCard
+                title="Total Monthy Price"
+                value={totalCount.monthyPrice}
+              />
             </CardGrid>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="d-flex justify-content-center">
+            <SearchNumbers onChange={onFiltering} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <NumbersTable
+              data={numbers?.data}
+              totalRows={numbers?.total}
+              handleChangePage={setPage}
+              progressPending={numbers?.loading}
+            />
           </Col>
         </Row>
       </Container>
