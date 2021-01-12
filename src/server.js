3c87d5b91ea5091889fb1f2ef5b9c4eda6f2a9e1
Weslen Breno <weslenbreno@gmail.com>
2021-01-11 22:18:55 -0300

    Add CRUD routes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
-import { createServer, Model, Factory } from 'miragejs';
+import { createServer, Model, Factory, RestSerializer, Response } from 'miragejs';
 import generateNumber  from 'utils/scripts/generateDIDNumbers';
+import { removeDIDFormat } from 'utils/scripts/helpers';
+
+const queryByValue = (arr = [], query = '') => {
+  return arr.filter(item => removeDIDFormat(item.value.toLowerCase()).includes(query.toLowerCase().replace(' ', '')))
+}
 
 export function makeServer({ environment = 'test' }) {
   return createServer({
@@ -37,13 +42,20 @@ export function makeServer({ environment = 'test' }) {
       number: Model,
     },
 
+    serializers: {
+      numbers: RestSerializer.extend({
+        include(request, resource) {
+          return Object.keys(this.schema.associationsFor(resource.modelName));
+        }
+      })
+    },
+
     routes() {
       this.namespace = 'api';
       this.get('numbers', (schema, request) => {
-        let {queryParams: { pageOffset = 1, pageSize }} = request
-        
-        const numbers = schema.db.numbers;
-
+        let {queryParams: { pageOffset = 1, pageSize, query = '' }} = request        
+        const numbers = queryByValue(schema.db.numbers, query);
+    
         if (parseInt(pageSize)) {
             
             const start = parseInt(pageSize) * (parseInt(pageOffset) <= 1 ? 0: pageOffset -1)
@@ -57,10 +69,47 @@ export function makeServer({ environment = 'test' }) {
                 prev: parseInt(pageOffset) - 1 === 0 ? null : parseInt(pageOffset) - 1
             }
         }
+
+
         return numbers
       });
       
-      this.resource("numbers", { except: ["index"] })
+      this.get("/numbers/:id");
+      this.post("/numbers", (schema, request) => {
+        let attrs = JSON.parse(request.requestBody)
+        const query = schema.db.numbers.where({value: attrs.value});
+      
+        if (query.length > 0) {
+          return new Response(409, {}, { statusCode:  409, error: 'Resource alredy exists'});
+        }
+        
+        const newNumber = schema.create('number', attrs)
+        attrs.id = newNumber.id;        
+        return new Response(200, {}, { statusCode: 200,  number:  attrs})
+      })
+
+      this.patch("/numbers/:id", (schema, request) => {
+        let id = request.params.id;
+        let attrs = JSON.parse(request.requestBody)
+
+        const query = schema.db.numbers.where({value: attrs.value})
+                                       .filter(number => number.id !== id);
+      
+        if (query.length > 0) {
+          return new Response(409, {}, { statusCode:  409, error: 'Resource with this number alredy exists'});
+        }
+        schema.numbers.find(id).update(attrs)
+        
+        return new Response(200, {}, { statusCode: 200,  number:  attrs, updated: false})
+      })
+
+      this.del("/numbers/:id", (schema, request) => {
+        let id = request.params.id;
+        const number = schema.numbers.find(id);
+        number.destroy();
+        return new Response(200, {}, { statusCode: 200,  number:  number, updated: true})
+      })
+
     },
   });
 }
