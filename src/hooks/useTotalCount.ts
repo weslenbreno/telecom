dd8c751dc07411d5a0c460f4deb9dd4bed9e51a8
Weslen Breno <weslenbreno@gmail.com>
2021-01-11 22:19:35 -0300

    Fix bug on change data

--- src/hooks/useTotalCount.ts
+++ src/hooks/useTotalCount.ts
@@ -1,33 +1,45 @@
-import {useEffect, useState} from 'react'
-import api  from 'config/api';
+import { selectNumbers } from 'ducks/numbersSlice';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import api from 'config/api';
 
 type CountNumbers = {
-    numbers: number | string,
-    monthyPrice: number | string,
-    setupPrice: number | string
-}
+  numbers: number | string;
+  monthyPrice: number | string;
+  setupPrice: number | string;
+};
 
 const useTotalCount = (): CountNumbers => {
-    const [total, setTotal] = useState<CountNumbers>({
-        numbers: 0,
-        monthyPrice: 0,
-        setupPrice: 0
-    });
+  const numbers = useSelector(selectNumbers);
 
-    useEffect(() => {
-        api('numbers').then((numbers: any[]) => {
-            const countSetup = numbers.reduce((accumulator: number, item: any) => accumulator + parseFloat(item.setupPrice), 0);
-            const countMonthy = numbers.reduce((accumulator: number, item: any) => accumulator + parseFloat(item.monthyPrice), 0);
+  const [total, setTotal] = useState<CountNumbers>({
+    numbers: 0,
+    monthyPrice: 0,
+    setupPrice: 0,
+  });
 
-            setTotal({
-                numbers: numbers.length,
-                setupPrice: countSetup.toFixed(2),
-                monthyPrice: countMonthy.toFixed(2)
-            });
+  useEffect(() => {
+      api('numbers').then((numbers: any[]) => {
+        const countSetup = numbers.reduce(
+          (accumulator: number, item: any) =>
+            accumulator + parseFloat(item.setupPrice),
+          0
+        );
+        const countMonthy = numbers.reduce(
+          (accumulator: number, item: any) =>
+            accumulator + parseFloat(item.monthyPrice),
+          0
+        );
+
+        setTotal({
+          numbers: numbers.length,
+          setupPrice: countSetup.toFixed(2),
+          monthyPrice: countMonthy.toFixed(2),
         });
-    }, [])
-        
-    return total;
-}
+      });
+  }, [numbers.status]);
+
+  return total;
+};
 
 export default useTotalCount;
